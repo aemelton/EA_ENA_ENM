b844a71bb5a91c3d21f1b18416805a03828f038d
Anthony E. Melton <anthonymelton@boisestate.edu>
2020-09-29 19:39:42 -0600

    Add files via upload
    Cleaned up GetOccurrence script - removed defunct parts of script

--- Functions/GetOccurrenceData.R
+++ Functions/GetOccurrenceData.R
@@ -11,18 +11,15 @@
 #
 
 GetOccurrenceData <- function(method = "scrubr", usr.query, usr.db, usr.output){
-	print("This function currently uses scrubr to clean data. Later additions will expand upon just running a bunch of commands together. Maybe...")
-	
+
 	if(method == "scrubr"){
 	spocc <- occ(query = usr.query, from = usr.db, has_coords = T)
   	
-  	spocc <- fixnames(spocc, how = "query")
+  spocc <- fixnames(spocc, how = "query")
 
 	allSpocc <- occ2df(spocc)
 	
 	allSpocc <- date_standardize(allSpocc, "%d%b%Y")
-	
-	paste("Raw points from spocc query: ", nrow(allSpocc), " points.", sep = "")
 
 	#Step 1: Using scrubr
 	scrubbed <- coord_incomplete(allSpocc) # Removes data with incomplete coordinates
@@ -35,29 +32,6 @@ GetOccurrenceData <- function(method = "scrubr", usr.query, usr.db, usr.output){
 	scrubbed <- scrubbed[,-6] # Removes unique, database-specific keys
 	
 	unique <- unique(scrubbed[,-4]) # Removes duplicate points (not considering data provider)
-
-	paste("Step 2: Removing duplicate dates and localities: ", nrow(unique), " points left.", sep = "")
-
-	#Step 3: Removing points with no environmental data
-#	G_SPECIESExtract <- extract(usr.ref, sapply(unique[2:3], as.numeric))
-
-#	G_SPECIESExtract<- cbind(unique, G_SPECIESExtract)
-
-	#cleanExtract <- G_SPECIESExtract[complete.cases(G_SPECIESExtract[,5]),]
-
-#	paste("Step 3: Remove points with no environmental data: ", nrow(cleanExtract), " points left.", sep = "")
-
-#	#Step 4: Reduce points to resolution of reference raster
-#	rasterResolution <- max(res(usr.ref))
-#
-#	while(min(nndist(cleanExtract[,2:3])) < rasterResolution){
-# 		nnD <- nndist(cleanExtract[,2:3])
-#  		cleanExtract <- cleanExtract[-(which(min(nnD) == nnD)[1]),]
-#		}
-#
-#	row.names(cleanExtract) <- seq(nrow(cleanExtract))
-#
-#	paste("Step 4: Resolution reduction: ", nrow(cleanExtract), " points left.", sep = "")
 
 	write.csv(unique[,1:4], file = usr.output, row.names = F)
 	}
