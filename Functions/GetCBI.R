94e5cf95644dcd8dc110711dad5820b93fe871ee
Anthony E. Melton <anthonymelton@boisestate.edu>
2020-11-13 20:40:27 -0700

    Add files via upload
    Fixed parameter in function. Was calling on the proper object, but didn't have the flag I wanted in function.

--- Functions/GetCBI.R
+++ Functions/GetCBI.R
@@ -8,7 +8,7 @@ mod.num <- mod.num #
 mod.settings <- results$settings[[mod.num]]
 usr.proj <- predict(modeval.usr.sp@models[[mod.num]], red.envStack)
     
-cbi.training <- ecospat.boyce(fit = usr.proj, obs = occ.dat, PEplot = F, nclass = nclass, window.w = window.w, res = res)
+cbi.training <- ecospat.boyce(fit = usr.proj, obs = usr.occs, PEplot = F, nclass = nclass, window.w = window.w, res = res)
 enmeval.cbi.training <- data.frame(cbi.training$Spearman.cor)
 CBI <- cbind(mod.num, mod.settings, enmeval.cbi.training)#, avg.cbi.testing, enmeval.cbi.testing1, enmeval.cbi.testing2, enmeval.cbi.testing3, enmeval.cbi.testing4, enmeval.cbi.testing5)
 }
